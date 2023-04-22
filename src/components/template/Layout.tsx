5797b23bdb40c72517645a7252c24e70298694d0
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-04-22 10:38:51 -0300

    configurando dark e light modo do tailwind

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -12,9 +12,11 @@ interface LayoutProps{
 
 export default function Layout(props:LayoutProps){
     return(
-        <div className={`flex h-screen w-screen`}>
+        //para configurar o black e light modo e so colocar o dark na classe principal e todos dentro deve ter ter os components com 
+        //nome dark: bg-color-white e então quando aparecer o dark na div acima todos esses itens com dark irao ativar
+        <div className={`dark flex h-screen w-screen`}>
             <Aside/>
-            <div className={`flex flex-col w-full p-7 bg-gray-300`}>
+            <div className={`flex flex-col w-full p-7 bg-gray-300 dark:bg-gray-800`}>
                 <Header title={props.title} subTitle={props.subTitle}></Header>
                 <Content> {props.children}</Content>
             </div>
